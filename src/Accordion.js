34ab54f0582b59d37a47276d705af8e40c5bfd42
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-04-22 15:17:23 +0200

    Updated single selection to multi selection

--- src/Accordion.js
+++ src/Accordion.js
@@ -3,21 +3,31 @@ import data from "./data";
 import "./styles.css";
 
 export default function Accordion() {
-  const [selected, setSelected] = useState(null);
-  const toggle = (id) => {
-    setSelected(selected === id ? null : id);
-  };
+  //const [selected, setSelected] = useState(null);
+  const [selectedItems, setSelectedItems] = useState([]);
+  // const toggle = (id) => {
+  //   setSelected(selected === id ? null : id);
+  // };
+  function toggleItem(id) {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter((itemId) => itemId !== id));
+      //This removes if already selected.
+    } else {
+      setSelectedItems([...selectedItems, id]);
+      //This adds if not selected.
+    }
+  }
   return (
     <div className="wrapper">
       <div className="accordion">
         {data.map((item) => (
           <div className="item" key={item.id}>
-            <div className="title" onClick={() => toggle(item.id)}>
+            <div className="title" onClick={() => toggleItem(item.id)}>
               <h3 className="question">{item.question}</h3>
-              <span>{selected === item.id ? "-" : "+"}</span>
-              <div className={`answer ${selected === item.id ? "show" : ""}`}>
-                {item.answer}
-              </div>
+              <span>{selectedItems === item.id ? "-" : "+"}</span>
+              {selectedItems.includes(item.id) && (
+                <div className="content">{item.answer}</div>
+              )}
             </div>
           </div>
         ))}
